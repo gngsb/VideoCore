7feed13ab0d0c4dde52d88da61ad990e008d47ea
liuxt <1738997412@qq.com>
2021-01-13 13:54:35 +0800

    add

---
+++ VideoManage/Controllers/VideoManageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoManage.Constants;
+using VideoManage.EFCore;
+using VideoManage.EFCore.Models;
+using VideoManage.Service.Video;
+
+namespace VideoManage.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VideoManageController : ControllerBase
+    {
+        private readonly VideoService _service;
+
+        public VideoManageController(VideoService service) 
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// 获取视频列表
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public PageApiResult<VVideolist> GetVideo(PageQuery query) 
+        {
+            return _service.GetVideo(query);
+        }
+    }
+}
---
+++ VideoManage/Startup.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoManage.EFCore;
+using Microsoft.OpenApi.Models;
+using VideoManage.Service.Video;
+using System.Reflection;
+using System.IO;
+
+namespace VideoManage
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddScoped<DbContext, VideoContext>();
+            services.AddScoped<VideoService>();
+            services.AddDbContext<VideoContext>(options => options.UseMySql(Configuration.GetConnectionString("Default")));
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "视频管理系统", Version = "v1" });
+                    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                    // 启用xml注释. 该方法第二个参数启用控制器的注释，默认为false
+                    c.IncludeXmlComments(xmlPath);
+                }
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //跨域访问
+            app.UseCors();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
